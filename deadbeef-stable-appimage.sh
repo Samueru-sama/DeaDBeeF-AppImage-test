3625b423121322d6ff0782de0c4ffaad2a82bbde
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-04-27 03:47:26 -0400

    refactor

--- deadbeef-stable-appimage.sh
+++ deadbeef-stable-appimage.sh
@@ -1,36 +1,47 @@
 #!/bin/sh
 
-set -eu
-
-APP=DeaDBeeF
-SITE="$(wget -q https://sourceforge.net/projects/deadbeef/files/travis/linux/ -O - \
-  | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | grep download | grep -vE 'master|feature|bugfix|1.10' | head -1 | sed 's/download//g')"
-TARGET_BIN="deadbeef"
-DESKTOP="https://raw.githubusercontent.com/DeaDBeeF-Player/deadbeef/master/deadbeef.desktop.in"
-ICON="https://raw.githubusercontent.com/DeaDBeeF-Player/deadbeef/master/icons/scalable/deadbeef.svg"
+set -ex
 
 export ARCH="$(uname -m)"
 export APPIMAGE_EXTRACT_AND_RUN=1
 
-APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage"
+APPIMAGETOOL="https://github.com/pkgforge-dev/appimagetool-uruntime/releases/download/continuous/appimagetool-$ARCH.AppImage"
 UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|continuous|*$ARCH.AppImage.zsync"
-LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
+
+if [ "$1" = 'devel' ]; then
+	echo "Making nightly release..."
+	APP=DeaDBeeF_Nightly
+	SITE="https://sourceforge.net/projects/deadbeef/files/travis/linux/master"
+else
+	echo "Making stable release..."
+	APP=DeaDBeeF
+	SITE=$(wget "https://sourceforge.net/projects/deadbeef/files/travis/linux" -O - \
+		| sed 's/[()",{} ]/\n/g' | grep -o 'https.*linux.*download$' \
+		| grep -vi 'master\|feature\|bugfix' | head -1 | sed 's|/download||')
+fi
+
+TARGET_BIN="deadbeef"
+DESKTOP="https://raw.githubusercontent.com/DeaDBeeF-Player/deadbeef/master/deadbeef.desktop.in"
+ICON="https://raw.githubusercontent.com/DeaDBeeF-Player/deadbeef/master/icons/scalable/deadbeef.svg"
+TARBALL=$(wget "$SITE" -O - | sed 's/[()",{} ]/\n/g' \
+	| grep -o "https.*linux.*$ARCH.tar.bz2.*download$" | head -1)
+
+# FIXME
+export VERSION=$(echo "$TARBALL" | awk -F'_' '{print $2; exit}')
 
 # Prepare AppDir
-mkdir -p "$APP"/AppDir/usr/share/applications
-cd "$APP"/AppDir
-url="$(wget -q "$SITE" -O - | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u | grep "x86_64.tar.bz2")"
-wget $url -O download.tar.bz2
-tar fx ./*.tar*
-rm -f ./*.tar*
+mkdir ./AppDir
+cd ./AppDir
 
-mv ./deadbeef* ./usr/bin
-mv ./usr/bin/lib ./usr/lib
+wget "$TARBALL" -O download.tar.bz2
+tar xvf ./*.tar.*
+rm -f ./*.tar.*
+
+mv -v ./deadbeef* ./bin
+mv -v ./bin/lib   ./
 
 wget "$DESKTOP" -O ./"$APP".desktop
 wget "$ICON" -O ./deadbeef.svg
-export VERSION="$(echo "$url" | awk -F"_" '{print $2; exit}')"
-cp ./"$APP".desktop ./usr/share/applications
 
 # remove all traces of gtk2
 echo "Deleting GTK2..."
@@ -39,62 +50,49 @@ find . -type f -iname '*gtk2*' -delete
 echo "-------------------------------------------------------------------"
 
 # Deploy all libs
-cp -vn /usr/lib/libgtk-*     ./usr/lib
-cp -rv /usr/lib/alsa-lib     ./usr/lib
-cp -rv /usr/lib/pipewire-0.3 ./usr/lib
-cp -rv /usr/lib/spa-0.2      ./usr/lib
+cp -vn /usr/lib/libgtk-*      ./lib
+cp -rv /usr/lib/gdk-pixbuf-*  ./lib
+cp -rv /usr/lib/alsa-lib      ./lib
+cp -rv /usr/lib/pipewire-*    ./lib
+cp -rv /usr/lib/spa-*         ./lib
+cp -vn /lib64/ld-linux*.so.*  ./ld-linux.so
 
-ldd ./usr/lib/* ./usr/lib/alsa-lib/* 2>/dev/null \
-	| awk -F"[> ]" '{print $4}' | xargs -I {} cp -vn {} ./usr/lib || true
-ldd ./usr/bin/deadbeef 2>/dev/null \
-	| awk -F"[> ]" '{print $4}' | xargs -I {} cp -vn {} ./usr/lib
-ldd ./usr/bin/plugins/* 2>/dev/null \
-	| awk -F"[> ]" '{print $4}' | xargs -I {} cp -vn {} ./usr/lib || true
+ldd ./lib/* \
+	./lib/alsa-lib/* \
+	./bin/deadbeef \
+	./bin/plugins/* 2>/dev/null \
+	| awk -F"[> ]" '{print $4}' \
+	| xargs -I {} cp -vn {} ./lib || true
 
-cp -vn /lib64/ld-linux-x86-64.so.2 ./usr/lib
-
-# DEPLOY GDK
-echo "Deploying gdk..."
-GDK_PATH="$(find /usr/lib -type d -regex ".*/gdk-pixbuf-2.0" -print -quit)"
-cp -rv "$GDK_PATH" ./usr/lib
-
-echo "Deploying gdk deps..."
-find ./usr/lib/gdk-pixbuf-2.0 -type f -name '*.so*' -exec ldd {} \; \
-	| awk -F"[> ]" '{print $4}' | xargs -I {} cp -vn {} ./usr/lib
-find ./usr/lib -type f -regex '.*gdk.*loaders.cache' \
+# deploy gdk
+find ./lib/gdk-pixbuf-* -type f -name '*.so*' -exec ldd {} \; \
+	| awk -F"[> ]" '{print $4}' | xargs -I {} cp -vn {} ./lib
+find ./lib -type f -regex '.*gdk.*loaders.cache' \
 	-exec sed -i 's|/.*lib.*/gdk-pixbuf.*/.*/loaders/||g' {} \;
-
-( cd ./usr/lib && find ./*/* -type f -regex '.*\.so.*' -exec ln -s {} ./ \; )
+( cd ./lib && find ./*/* -type f -regex '.*\.so.*' -exec ln -s {} ./ \; )
 
 # Create AppRun
 echo '#!/bin/sh
 CURRENTDIR="$(readlink -f "$(dirname "$0")")"
-GDK_HERE="$(find "$CURRENTDIR" -type d -regex '.*gdk.*loaders' -print -quit)"
-GDK_LOADER="$(find "$CURRENTDIR" -type f -regex '.*gdk.*loaders.cache' -print -quit)"
 
-export GDK_PIXBUF_MODULEDIR="$GDK_HERE"
-export GDK_PIXBUF_MODULE_FILE="$GDK_LOADER"
-export SPA_PLUGIN_DIR="$CURRENTDIR/usr/lib/spa-0.2"
-export PIPEWIRE_MODULE_DIR="$CURRENTDIR/usr/lib/pipewire-0.3"
+export GDK_PIXBUF_MODULEDIR="$(echo "$CURRENTDIR"/lib/gdk-pixbuf-*)"
+export GDK_PIXBUF_MODULE_FILE="$(echo "$GDK_PIXBUF_MODULEDIR"/*/loaders.cache)"
+export SPA_PLUGIN_DIR="$(echo "$CURRENTDIR"/lib/spa-*)"
+export PIPEWIRE_MODULE_DIR="$(echo "$CURRENTDIR"/lib/pipewire-*)"
 
-exec "$CURRENTDIR"/usr/lib/ld-linux-x86-64.so.2 \
-	--library-path "$CURRENTDIR"/usr/lib \
-	"$CURRENTDIR"/usr/bin/deadbeef "$@"' > ./AppRun
+exec "$CURRENTDIR"/ld-linux.so \
+	--library-path "$CURRENTDIR"/lib "$CURRENTDIR"/bin/deadbeef "$@"' > ./AppRun
 chmod +x ./AppRun
 
 # Strip everything
-find ./usr -type f -exec strip -s -R .comment --strip-unneeded {} ';'
+find ./ -type f -exec strip -s -R .comment --strip-unneeded {} ';'
 
 # MAKE APPIAMGE WITH FUSE3 COMPATIBLE APPIMAGETOOL
 cd ..
 wget -q "$APPIMAGETOOL" -O ./appimagetool
 chmod +x ./appimagetool
 
-./appimagetool --comp zstd \
-	--mksquashfs-opt -Xcompression-level --mksquashfs-opt 22 \
-	-n -u "$UPINFO" "$PWD"/AppDir "$PWD"/"$APP"-"$VERSION"-anylinux-"$ARCH".AppImage
+./appimagetool -n -u "$UPINFO" \
+	"$PWD"/AppDir "$PWD"/"$APP"-"$VERSION"-anylinux-"$ARCH".AppImage
 
-mv ./*.AppImage* ../
-cd ..
-rm -rf ./"$APP"
 echo "All Done!"
